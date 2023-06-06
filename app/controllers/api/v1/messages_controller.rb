a4d50d7f9085162241d2ab9ed9574bd0f3b3ea08
Lancelot-SO <fsowah001@gmail.com>
2023-06-06 14:33:01 +0000

    made chages

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class MessagesController < ApplicationController
       def index
-        @messages = Message.all
+        @messages = Message.all.sample
         render json: @messages, status: :ok
       end
     end
