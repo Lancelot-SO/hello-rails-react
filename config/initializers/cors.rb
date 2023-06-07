643f1d3d359838c147b630a495c702047f8fbe2e
Lancelot-SO <fsowah001@gmail.com>
2023-06-07 08:57:58 +0000

    resolve changes requested

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins 'http://localhost:5000' , 'http://localhost:3000' # Update with your frontend origin
-      resource '*', headers: :any, methods: [:get] # Update with the desired HTTP methods
-    end
+  allow do
+    origins '*'
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
-  
